e9490c9f28ee34128eee20a70b55572ac9e0cf11
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-05 12:55:16 -0500

    added float left to form and current button

--- src/Search.js
+++ src/Search.js
@@ -3,22 +3,24 @@ import React from "react";
 export default function Search() {
   return (
     <div className="clearfix">
-      <form className="float-left search-form mb-3" id="search-form">
+      <form className="float-left mb-3">
         <input
           type="search"
           placeholder="Type a city.."
           autoFocus="on"
           autoComplete="off"
           id="city-input"
-          className="shadow-sm"
+          className=" form-control-sm shadow-sm"
         />
         <input
           type="submit"
           value="Search"
-          className="btn btn-primary shadow-sm"
+          className="form-control-sm btn btn-primary shadow-sm"
         />
       </form>
-      <button className="float-left btn btn-success shadow-sm">Current</button>
+      <button className="float-left form-control-sm btn btn-success shadow-sm">
+        Current
+      </button>
     </div>
   );
 }
