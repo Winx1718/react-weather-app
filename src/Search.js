8022afc56289f1a9de6625b4295743d85c299d36
Winx1718 <98168228+Winx1718@users.noreply.github.com>
2022-07-06 13:30:13 -0500

    fixed spacing

--- src/Search.js
+++ src/Search.js
@@ -2,25 +2,34 @@ import React from "react";
 
 export default function Search() {
   return (
-    <div className="clearfix">
-      <form className="float-left mb-3">
-        <input
-          type="search"
-          placeholder="Type a city.."
-          autoFocus="on"
-          autoComplete="off"
-          id="city-input"
-          className=" form-control-sm shadow-sm"
-        />
-        <input
-          type="submit"
-          value="Search"
-          className="form-control-sm btn btn-primary shadow-sm"
-        />
-      </form>
-      <button className="float-left form-control-sm btn btn-success shadow-sm">
-        Current
-      </button>
-    </div>
+    <form className="mb-3">
+      <div className="row">
+        <div className="col-6">
+          <input
+            type="search"
+            placeholder="Type a city.."
+            autoFocus="on"
+            autoComplete="off"
+            id="city-input"
+            className=" form-control shadow-sm"
+          />
+        </div>
+        <div className="col-3">
+          <input
+            type="submit"
+            value="Search"
+            className="form-control btn btn-primary shadow-sm"
+          />
+        </div>
+        <div className="col-3">
+          <button
+            type="button"
+            className="float-left form-control btn btn-success shadow-sm"
+          >
+            Current
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
